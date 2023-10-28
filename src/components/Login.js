723313272df16eb039c35ef2dc5a32ac48cbf424
Pankaj yadav <abhi7868090@gmail.com>
2023-10-28 13:13:27 +0530

    create form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.scss";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "../firebaseConfig";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import backgroundImage from "./banner.jpg";
 import { Link, useNavigate } from "react-router-dom";
 
-const Login = ({page}) => {
+const Login = ({ page }) => {
   const app = initializeApp(firebaseConfig);
 
   // const location = useLocation(); // it is already provide pathname="/login" true
@@ -15,8 +19,30 @@ const Login = ({page}) => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isUserExist, setUserExist] = useState(false)
-  const [isUserAlready, setUserAlready] = useState(false)
+  const [isUserExist, setUserExist] = useState(false);
+  const [isUserAlready, setUserAlready] = useState(false);
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
+
+  const validation = (fieldName, value) => {
+    switch (fieldName) {
+      case "email":
+        return value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+      case "password":
+        return value.length >= 6;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    setEmail("")
+    setPassword("")
+    setUserExist(false);
+    setUserAlready(false);
+    setEmailValid(true)
+    setPasswordValid(true)
+  }, [page]);
 
   const emailOnChangeHandler = (e) => {
     setEmail(e.target.value);
@@ -25,29 +51,37 @@ const Login = ({page}) => {
   const passwordOnChangeHandler = (e) => {
     setPassword(e.target.value);
   };
- 
+
   const navigate = useNavigate();
+
   const auth = getAuth();
   // console.log(auth)
 
   const onSignInClickHandler = (e) => {
     e.preventDefault();
-    if(page){
-    signInWithEmailAndPassword(auth, email, password)
-      .then((auth) => {
-        if (auth) {
-          navigate('/home');
-        }
-      })
-      .catch((error) => setUserExist(true));
+
+    if (!validation("email", email) || !validation("password", password)) {
+      setEmailValid(validation("email", email));
+      setPasswordValid(validation("password", password));
+      return;
+    }
+
+    if (page) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((auth) => {
+          if (auth) {
+            navigate("/home");
+          }
+        })
+        .catch((error) => setUserExist(true));
     } else {
       createUserWithEmailAndPassword(auth, email, password)
-      .then((auth) => {
-         if(auth){
-          navigate('/home')
-         }
-      })
-      .catch((error) => setUserAlready(true))
+        .then((auth) => {
+          if (auth) {
+            navigate("/home");
+          }
+        })
+        .catch((error) => setUserAlready(true));
     }
   };
   return (
@@ -56,9 +90,9 @@ const Login = ({page}) => {
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="signIn-form">
-   {isUserExist && <h2>User does not exist | Go to SignUp</h2> }
-   {isUserAlready && <h2>Email already in use | Go for Sign In </h2> }
-        <h1>{page?"Sign In" : "Register"}</h1>
+        {isUserExist && <h2>User does not exist | Go to SignUp</h2>}
+        {isUserAlready && <h2>Email already in use | Go for Sign In </h2>}
+        <h1>{page ? "Sign In" : "Register"}</h1>
         <form>
           <input
             type="text"
@@ -66,24 +100,29 @@ const Login = ({page}) => {
             onChange={emailOnChangeHandler}
             placeholder="Email or phone number"
           />
+          {!emailValid && <p>Email is invalid/blank</p>}
           <input
             type="password"
             value={password}
             onChange={passwordOnChangeHandler}
             placeholder="Password"
           />
+          {!passwordValid && <p>Email is invalid/blank</p>}
           <button className="button" onClick={onSignInClickHandler}>
-          {page?"Sign In" : "Register"}
+            {page ? "Sign In" : "Register"}
           </button>
         </form>
         <div className="para">
           <h3>
-            {page ? "New to Netflix" : "Existing User"} <Link to={page ? "/register" : "/login"}><span>{page ? "Sign up now." : "Sign In"}</span></Link>
+            {page ? "New to Netflix" : "Existing User"}{" "}
+            <Link to={page ? "/register" : "/login"}>
+              <span>{page ? "Sign up now." : "Sign In"}</span>
+            </Link>
           </h3>
           <p>
             This page is protected by Google reCAPTCHA to ensure you're not a
             bot.
-            <a href="##" style={{ color: "blue" }}>
+            <a href="/" style={{ color: "blue" }}>
               Learn more.
             </a>
           </p>
