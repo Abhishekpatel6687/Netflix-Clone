e7f653ec5f8eaf134524f72da1a014452df524ec
Pankaj yadav <abhi7868090@gmail.com>
2023-10-28 23:26:30 +0530

    responsive homeBanner

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,40 +1,46 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./HomeBanner.scss";
-import backgroundImage from "./banner.jpg"
+import backgroundImage from "./banner.jpg";
 const HomeBanner = () => {
   const navigate = useNavigate();
   const onSignInHandler = (e) => {
-    e.preventDefault()
-    navigate("/login")
-  }
+    e.preventDefault();
+    navigate("/login");
+  };
   return (
-    <>
-    <div className="container" style={{ backgroundImage: `url(${backgroundImage})` }} >
-      <div className="header-banner"> 
-        <img src="/Images/logo.png" alt="logo" />
-        <form>
-          <select className="option">
-            <option>Hindi</option>
-            <option>English</option>
-          </select>
-          <Link to="/login">
-          <button className="btn" onClick={onSignInHandler}>Sign In</button>
-          </Link>
-        </form>
-      </div>
-      <div className="home-banner">
-        <div>
-          <h1>Unlimited movies, TV shows and more.</h1>
-          <h3>Watch anywhere. Cancel anytime.</h3>
-          <p>Ready to watch? Enter your email to create or restart your membership.</p>
+      <div
+        className="container"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className="header-banner">
+          <img src="/Images/logo.png" alt="logo" />
+          <form>
+            <select className="option">
+              <option>Hindi</option>
+              <option>English</option>
+            </select>
+            <Link to="/login">
+              <button className="btn" onClick={onSignInHandler}>
+                Sign In
+              </button>
+            </Link>
+          </form>
+        </div>
+        <div className="home-banner">
           <div>
-            <input type="text" placeholder="Email Address" />
-            <button className="button">Get Started</button>
+            <h1>Unlimited movies, TV shows and more.</h1>
+            <h3>Watch anywhere. Cancel anytime.</h3>
+            <p>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </p>
+            <div>
+              <input type="text" placeholder="Email Address" />
+              <button className="button">Get Started</button>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-    </>
   );
 };
 
